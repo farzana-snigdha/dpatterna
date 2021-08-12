598b9fdbe1fca6bec6db98c0082294499f857bdd
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-08-12 19:50:49 +0600

    Create command-lightChange.ts

---
+++ src/patterns/command/command-lightChange.ts
@@ -0,0 +1,89 @@
+export class Light {
+  on(): string {
+    return "on";
+  }
+  off(): string {
+    return "off";
+  }
+}
+
+export class RedLight {
+  increase: number = 0;
+  on(): string {
+    return "red0";
+  }
+ 
+  increaseRedLight(): string {
+    this.increase++;
+    return `red${this.increase}`;
+  }
+  decreaseRedLight(): string {
+    this.increase--;
+    if(this.increase<0){
+        return 'off'
+    }
+    else{
+        return `red${this.increase}`;
+    }
+   
+  }
+}
+
+
+//commands
+export interface Command {
+  setCommand(): string;
+}
+
+export class TurnOnLightCommand implements Command{
+    light!: Light;
+
+    setCommand(): string{
+        return this.light.on()
+    }
+}
+
+export class TurnOffLightCommand implements Command{
+    light!: Light;
+
+    setCommand(): string{
+        return this.light.off()
+    }
+}
+
+export class TurnOnRedLightCommand implements Command{
+    light!: RedLight;
+
+    setCommand(): string{
+        return this.light.on()
+    }
+}
+
+export class IncreaseRedLightCommand implements Command{
+    light!: RedLight;
+
+    setCommand(): string{
+        return this.light.increaseRedLight()
+    }
+}
+
+export class DecreaseRedLightCommand implements Command{
+    light!: RedLight;
+
+    setCommand(): string{
+        return this.light.decreaseRedLight()
+    }
+}
+
+export class RemoteControl{
+    command:Command
+    constructor(command:Command){
+        this.command=command
+    }
+    executeCommand(){
+        return this.command.setCommand()
+    }
+
+}
+
+
